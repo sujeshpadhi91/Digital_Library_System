a4ec4e18bba3eeb673b6db0ac34db9ed3717eaa6
SUJESH PADHI <59525166+sujeshpadhi91@users.noreply.github.com>
2022-11-24 16:36:38 -0700

    Update librarian_agent.java
    manual update on counter

--- librarian_agent.java
+++ librarian_agent.java
@@ -1,6 +1,7 @@
 import jade.core.Agent;
 public class librarian_agent extends Agent {
 
+    private int counter = 0;
     protected void setup() {
         System.out.printf("My name is %s%n", getLocalName());
 
