0c0ccfea2a2cd7ae0772fd6b514f5ef6363f02c3
Sujesh Padhi <sujesh.padhi1@ucalgary.ca>
2022-11-24 17:47:13 -0700

    added a SimpleBehaviour

--- librarian_agent.java
+++ librarian_agent.java
@@ -5,6 +5,11 @@ public class librarian_agent extends Agent {
     protected void setup() {
         System.out.printf("My name is %s%n", getLocalName());
 
+        addBehaviour(new SimpleBehaviour(this) {
+            public void action() {
+
+            }
+        }
     }
 
 }
